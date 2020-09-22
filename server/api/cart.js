f1ebf883b64b70e19861053d29cb4d81e208bc78
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-22 12:46:32 -0400

    adds cart.js router to server/api and mounts the router in server/api/index.js; adds GET /api/cart/:userId to serve up products in a users cart from db

---
+++ server/api/cart.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const {Op} = require('sequelize')
+const {Cart, Product} = require('../db/models')
+module.exports = router
+
+// GET /api/cart/:userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const cart = await Cart.findOne({
+      where: {userId: req.params.userId}
+    })
+    // below conditional is added because cart is null for users I haven't seeded a cart for
+    // we should probably initilize an empty cart for every user when the user is created when we get there
+    // when that is done, this conditional can be removed (because cart will always exist)
+    if (!cart) {
+      res.json('no items in cart')
+      return
+    }
+
+    const productsInCart = await Product.findAll({
+      where: {
+        id: {
+          [Op.in]: cart.contents
+        }
+      }
+    })
+
+    res.json(productsInCart)
+  } catch (err) {
+    next(err)
+  }
+})
