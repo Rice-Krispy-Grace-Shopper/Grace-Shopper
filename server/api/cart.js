de269ce2d1699014ab35aefce38d2e3bdbdc2a5f
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-22 12:56:07 -0400

    adds Cart link to navbar.js for both logged in and guest views

--- server/api/cart.js
+++ server/api/cart.js
@@ -9,9 +9,10 @@ router.get('/:userId', async (req, res, next) => {
     const cart = await Cart.findOne({
       where: {userId: req.params.userId}
     })
+
     // below conditional is added because cart is null for users I haven't seeded a cart for
     // we should probably initilize an empty cart for every user when the user is created when we get there
-    // when that is done, this conditional can be removed (because cart will always exist)
+    // when that is done, this conditional can be removed (because cart will always exist for a logged in user)
     if (!cart) {
       res.json('no items in cart')
       return
