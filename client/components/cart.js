85a67299c0a2f24a9a82ce34b643ba530cb9bc08
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-22 13:39:45 -0400

    creates cart subreducer and maps thunk creator to Cart component; cart component puts user and cart contents onto state when component mounts; successfully rendering cart components to cart view for logged in users by mapping over cart on state

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {me} from '../store/user'
+import {getCart} from '../store/cart'
 
 class Cart extends Component {
+  async componentDidMount() {
+    await this.props.getUser()
+    await this.props.getCart(this.props.user.id)
+  }
+
   render() {
-    return <h1>Cart</h1>
+    const cart = this.props.cart
+    console.log('user on state-->', this.props.user)
+    return (
+      <React.Fragment>
+        <h1>Cart</h1>
+        {cart ? (
+          <ul>{cart.map(item => <li key={item.id}>{item.name}</li>)}</ul>
+        ) : (
+          'no items in cart'
+        )}
+      </React.Fragment>
+    )
   }
 }
 
-export default Cart
+const mapState = state => ({
+  cart: state.cart.cart,
+  user: state.user
+})
+
+const mapDispatch = dispatch => ({
+  getUser: () => dispatch(me()),
+  getCart: userId => dispatch(getCart(userId))
+})
+
+export default connect(mapState, mapDispatch)(Cart)
