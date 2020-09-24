572eec8fd12891f9d9988331715b48ead0d86782
Corey Mertz <CoreyMertz97@ATT.net>
2020-09-24 12:00:59 -0500

    Added Admin routes for Users, Still showing 401 Error in Postman.

--- server/api/users.js
+++ server/api/users.js
@@ -2,7 +2,18 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+//Admin Only Routes
+const isAdminMiddleware = (req, res, next) => {
+  if (!req.user || !req.user.isAdmin) {
+    const error = new Error('You are not an Admin')
+    error.status = 401
+    next(error)
+  } else {
+    next()
+  }
+}
+//Get all Users
+router.get('/', isAdminMiddleware, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -13,5 +24,51 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+//Get Single User
+router.get('/:id', isAdminMiddleware, async (req, res, next) => {
+  try {
+    const userById = await User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(userById)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Add New User
+router.post('/', isAdminMiddleware, async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).json(newUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Update User
+router.put('/:id', isAdminMiddleware, async (req, res, next) => {
+  try {
+    const userById = await User.findByPk(req.params.id)
+    const updatedUser = await userById.update(req.body)
+    res.json(updatedUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Delete User
+router.delete('/:id', isAdminMiddleware, async (req, res, next) => {
+  try {
+    const userById = await User.findByPk(req.params.id)
+    userById.destroy()
+    res.status(204).end()
+  } catch (error) {
+    next(error)
   }
 })
