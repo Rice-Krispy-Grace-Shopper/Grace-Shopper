1a6137df58d25b8c7c924a6b2f7f20c7316f0b64
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-28 13:18:43 -0400

    feat: order history: completes vertical slice: order subreducer action/thunk created, OrderHistory component created and wired up and rendering some order info, need to display product names, order date and style

---
+++ client/components/order-history.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {me} from '../store/user'
+import {getOrders} from '../store/order'
+
+class OrderHistory extends Component {
+  async componentDidMount() {
+    await this.props.getUser()
+    await this.props.getOrders(this.props.user.id)
+  }
+
+  render() {
+    console.log('orders on state-->', this.props.orders)
+    return (
+      <div className="OrderHistoryDiv">
+        <h2>Your Orders:</h2>
+        {this.props.orders.length
+          ? this.props.orders.map(order => (
+              <div key={order.id} className="SingleOrderDiv">
+                {order.contents.map(orderItem => (
+                  <div key={orderItem.productId} className="SingleOrderItemDiv">
+                    <p>
+                      <strong>Qty:</strong> {orderItem.qty}
+                    </p>
+                    <p>
+                      <strong>Price:</strong> ${(orderItem.price / 100).toFixed(
+                        2
+                      )}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ))
+          : 'You do not have any orders!'}
+      </div>
+    )
+  }
+}
+
+const mapState = state => ({
+  user: state.user,
+  orders: state.orders
+})
+
+const mapDispatch = dispatch => ({
+  getUser: () => dispatch(me()),
+  getOrders: userId => dispatch(getOrders(userId))
+})
+
+export default connect(mapState, mapDispatch)(OrderHistory)
